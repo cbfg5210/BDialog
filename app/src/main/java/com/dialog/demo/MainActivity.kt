93beb2f02321893330e7edcf768f201d78893604
沉璧浮光 <120276969@qq.com>
2019-09-24 14:43:39 +0800

    dialog:disable generate BuildConfig, add demo

--- app/src/main/java/com/dialog/demo/MainActivity.kt
+++ app/src/main/java/com/dialog/demo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.dialog.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.dialog.BDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowDialog.setOnClickListener {
+            BDialog.get()
+                .init(object : BDialog.InitDialogListener {
+                    override fun initDialog(dialog: BDialog, builder: AlertDialog.Builder) {
+                        builder.setTitle("Test dialog")
+                        builder.setMessage("Hello world!Hello world!")
+                    }
+                })
+                .show(lifecycle, supportFragmentManager, "TestDialog")
+        }
     }
 }
