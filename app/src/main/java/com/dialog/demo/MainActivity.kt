40fc39dcdc37f5c6be949c24b8ec71c2ca23e8b5
沉璧浮光 <120276969@qq.com>
2019-09-30 17:02:12 +0800

    update demo

--- app/src/main/java/com/dialog/demo/MainActivity.kt
+++ app/src/main/java/com/dialog/demo/MainActivity.kt
@@ -1,55 +1,103 @@
 package com.dialog.demo
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.dialog.demo.dialog.DDialogFragment
-import com.dialog.demo.view.ViewFragment
+import androidx.fragment.app.DialogFragment
+import com.dialog.BDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_dialog_1.view.tvMessage
+import kotlinx.android.synthetic.main.layout_dialog_2.view.tvBtn
+import kotlinx.android.synthetic.main.layout_dialog_2.view.tvTitle
+import kotlinx.android.synthetic.main.layout_dialog_bottom.view.*
+import kotlinx.android.synthetic.main.layout_dialog_input.view.*
 
-class MainActivity : AppCompatActivity() {
-    private var currentFlag = -1
-
-    private val actionBar: ActionBar by lazy { supportActionBar!! }
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        showFragment(R.id.dialogDemo)
+        setContentView(R.layout.activity_main)
+
+        btnDialog1.setOnClickListener(this)
+        btnDialog2.setOnClickListener(this)
+        btnVDialog1.setOnClickListener(this)
+        btnVDialog2.setOnClickListener(this)
+        btnDialogInput.setOnClickListener(this)
+        btnDialogBottom.setOnClickListener(this)
     }
 
-    private fun showFragment(id: Int) {
-        if (currentFlag == id) {
-            return
-        }
-
-        currentFlag = id
-
-        val fragment: Fragment = when (id) {
-            R.id.dialogDemo -> {
-                actionBar.title = "重写 DialogFragment.onCreateDialog(...) 方法"
-                DDialogFragment()
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnDialog1 -> {
+                BDialog.get()
+                        .init { _: BDialog, builder: AlertDialog.Builder -> builder.setMessage("这是一个只有内容的 Dialog") }
+                        .show(lifecycle, supportFragmentManager, "Dialog_1")
             }
-            R.id.layoutDemo -> {
-                actionBar.title = "重写 DialogFragment.onCreateView(...) 方法"
-                ViewFragment()
+
+            R.id.btnDialog2 -> {
+                BDialog.get()
+                        .init { _: BDialog, builder: AlertDialog.Builder ->
+                            builder.setTitle("温馨提示")
+                                    .setMessage("这是一个带标题、内容、按钮的 Dialog")
+                                    .setPositiveButton("知道了", null)
+                        }
+                        .show(lifecycle, supportFragmentManager, "Dialog_2")
             }
-            else -> return
+
+            R.id.btnVDialog1 -> {
+                BDialog.get()
+                        .init(R.layout.layout_dialog_1) { _, view -> view.tvMessage.text = "这是一个只有内容的 Dialog" }
+                        .show(lifecycle, supportFragmentManager, "ViewDialog_1")
+            }
+
+            R.id.btnVDialog2 -> {
+                BDialog.get()
+                        .init(R.layout.layout_dialog_2) { dialog, view ->
+                            view.tvTitle.text = "温馨提示"
+                            view.tvMessage.text = "这是一个带标题、内容、按钮的 Dialog"
+                            view.tvBtn.text = "知道了"
+                            view.tvBtn.setOnClickListener { dialog.dismiss(lifecycle) }
+                        }
+                        .show(lifecycle, supportFragmentManager, "ViewDialog_2")
+            }
+
+            R.id.btnDialogInput -> {
+                BDialog.get()
+                        //通过 Bundle 传入外部数据,在 init 中取出使用
+                        .apply { args().putString("default", "找一个女票...") }
+                        .init(R.layout.layout_dialog_input) { dialog, view ->
+                            view.tvTitle.text = "许下你的愿望吧:"
+                            view.tvBtn.text = "许好了"
+
+                            //从 Bundle 中获取数据使用
+                            view.etMessage.hint = dialog.args().getString("default")
+
+                            view.tvBtn.setOnClickListener {
+                                val text = view.etMessage.text
+                                if (TextUtils.isEmpty(text)) {
+                                    Toast.makeText(this, "你没有许下愿望喔~", Toast.LENGTH_LONG).show()
+                                } else {
+                                    Toast.makeText(this, "你的愿望已被接收: $text", Toast.LENGTH_LONG).show()
+                                }
+                                dialog.dismiss(lifecycle)
+                            }
+                        }
+                        .show(lifecycle, supportFragmentManager, "ViewDialog_Input")
+            }
+
+            R.id.btnDialogBottom -> {
+                BDialog.get()
+                        .apply { setStyle(DialogFragment.STYLE_NO_TITLE, R.style.BLayoutDialogSlideInBottomTheme) }
+                        .init(R.layout.layout_dialog_bottom) { _, view ->
+                            view.tvOption1.setOnClickListener { Toast.makeText(this, "Click option1", Toast.LENGTH_SHORT).show() }
+                            view.tvOption2.setOnClickListener { Toast.makeText(this, "Click option2", Toast.LENGTH_SHORT).show() }
+                            view.tvOption3.setOnClickListener { Toast.makeText(this, "Click option3", Toast.LENGTH_SHORT).show() }
+                        }
+                        .show(lifecycle, supportFragmentManager, "ViewDialog_Bottom")
+            }
         }
-
-        supportFragmentManager.beginTransaction()
-                .replace(android.R.id.content, fragment)
-                .commit()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        showFragment(item.itemId)
-        return true
     }
 }
